f1da6d8b559093ba08199dcb8a51531c392980de
Kevin sanders <ksanders8413@yahoo.com>
2024-10-04 22:48:46 -0500

    changed cookie settings to production

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,14 +35,14 @@ const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: false,
-    // process.env.NODE_ENV === "development",
+    process.env.NODE_ENV === "production",
     sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: false,
-    // process.env.NODE_ENV === "development",
+    process.env.NODE_ENV === "production",
     sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
@@ -215,7 +215,7 @@ export const refreshToken = async (req, res) => {
 
 		res.cookie("accessToken", accessToken, {
 			httpOnly: true,
-			secure: process.env.NODE_ENV === "development",
+			secure: process.env.NODE_ENV === "production",
 			sameSite: "strict",
 			maxAge: 15 * 60 * 1000,
 		});
