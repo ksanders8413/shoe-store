bf13ce64a36d053d8a92c7e9ee4e73c3e0335e71
Kevin sanders <ksanders8413@yahoo.com>
2024-10-04 22:56:49 -0500

    fixed the cookies

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -34,15 +34,13 @@ const storeRefreshToken = async (userId, refreshToken) => {
 const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
-    secure: false,
-    process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 15 * 60 * 1000, // 15 minutes
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
-    secure: false,
-    process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "strict", // prevents CSRF attack, cross-site request forgery attack
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
